adfa964a3e5aa4846bfb3a495ced6b11f368d692
MarinaZinkova <mzinkova@softrust.ru>
2020-11-05 18:44:31 +0300

    Сортировка строк

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,26 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const arrNew = new Array();
+  for (let i = 0; i < arr.length; i++) {
+      let b = false;
+      for (let j = 0; j < arrNew.length; j++) {
+          const sr = arr[i].localeCompare(arrNew[j], ['ru', 'en'], { caseFirst: 'upper' });
+          if(sr < 1){
+              arrNew.splice(j, 0 , arr[i]);
+              b = true;
+              break;
+          }
+        }
+        if(!b){
+            arrNew.push(arr[i]);
+        }
+    }
 
-}
+    if(param == 'desc'){
+      return arrNew.reverse();
+    }
+    else{
+        return arrNew;          
+    }
+  }
