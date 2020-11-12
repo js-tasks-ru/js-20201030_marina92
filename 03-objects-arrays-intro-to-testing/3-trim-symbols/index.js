e7ab59b28c1119e3ab747611c1e8c934981ea57e
MarinaZinkova <mzinkova@softrust.ru>
2020-11-12 18:57:27 +0300

    Строка символов

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,20 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    let key;
+    let strTemp = '';
+    let res = '';
+    
+    for(let i = 0; i <= string.length; i++){
+      if(string[i] != key){
+        if(strTemp != ''){
+          res += strTemp.substr(0, size);
+        }
+        strTemp = '';
+        key = string[i];
+      }
+      strTemp += string[i];
+    }
 
+    return res;
 }
